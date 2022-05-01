374d43a7ef77e29eb1918e5d52951ec1c9c354f2
castobary <46651885+castobary@users.noreply.github.com>
2022-05-02 01:17:00 +0300

    Starter Plots

---
+++ plots.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
+
+# Graph Styling
+
+plt.style.use("seaborn-darkgrid")
+
+
+# Line Graphs
+
+# By default Plot() function will draw a line chart.
+
+x = np.array([1,2,3,4,5,6])
+y = np.power(x,3)
+
+plt.plot(x,y)
+plt.show()
+
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.show()
+
+# Recover default matplotlib settings
+mpl.rcParams.update(mpl.rcParamsDefault)
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.show()
+
+plt.style.use("seaborn-darkgrid")
+
+plt.figure(figsize=(10,5))
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.show()
+
+# Solid blue line will be plotted using the argument "b-"
+
+plt.figure(figsize=(10,5))
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+plt.plot(x,y, "b-")
+plt.xlabel('X - Axis')
+plt.ylabel('Y - Axis')
+plt.show()
+
+
+# Solid blue line will be plotted using the argument "r-"
+
+plt.figure(figsize=(10,5))
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+plt.plot(x,y, "r-")
+plt.xlabel('X - Axis')
+plt.ylabel('Y - Axis')
+plt.show()
