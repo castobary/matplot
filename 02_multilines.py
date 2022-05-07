e3be736785fc9561f7d8a9bf6e9005f43160934e
castobary <46651885+castobary@users.noreply.github.com>
2022-05-07 09:19:26 +0300

    Multilines complete :)

--- 02_multilines.py
+++ 02_multilines.py
@@ -58,5 +58,56 @@ plt.xlabel(r'$X$' , fontsize = 18)
 plt.ylabel(r'$Y$' , fontsize = 18)
 plt.title("$Sin(x) $ $ & $ $ Cos(x)$" ,fontsize = 14)
 plt.legend(loc = 'upper right' , 
-           fontsize = 8 , bbox_to_anchor=(1.2, 1.0))
+           fontsize = 8)
+plt.show()
+
+# Shading Regions with fill_between() function
+
+x = np.linspace(0, 10, 2000)
+plt.figure(figsize=(10,6))
+plt.plot(x,np.sin(x) , label = '$Sin(X)$')
+plt.plot(x,np.cos(x) , label = '$cos(X)$')
+plt.fill_between(x,0,np.sin(x))
+plt.fill_between(x,0,np.cos(x))
+plt.xlabel(r'$X$' , fontsize = 18)
+plt.ylabel(r'$Y$' , fontsize = 18)
+plt.title("$Sin(x) $ $ & $ $ Cos(x)$" ,fontsize = 14)
+plt.legend(loc = 'lower left') # Legend will be placed at lower left position 
+plt.show()
+
+#Changing Legend position & font
+
+x = np.array([1,2,3,4,5,6])
+y1 = np.power(x,2)
+y2 = np.power(x,3)
+plt.plot(x,y1, "b-" , label = '$y1 = x^2$') # Setting up legends 
+plt.plot(x,y2, "r-" ,label ='$y2 = x^3$') # Setting up legends 
+plt.xlabel("X - Axis")
+plt.ylabel("Y - Axis")
+plt.legend(loc='upper center', fontsize='large') 
+plt.show()
+
+# Changing line width
+plt.figure(figsize=(10,6))
+x= [1,2,3,4,5,6,7,8,9]
+y= [7,8,9,10,7,11,12,13,14]
+y2 = [7,7,7,7,7,7,7,7,7]
+plt.plot(x , y, linewidth = 4 ,label = 'Line -1') # Changing line width 
+plt.plot(x , y2, linewidth = 3,label = 'Line - 2')
+plt.xlabel('X Axis Label')
+plt.ylabel('Y Axis Label')
+plt.title ('Line Graph')
+plt.legend()
+plt.show()
+
+# Setting the background color
+x = np.array([1,2,3,4,5,6])
+y1 = np.power(x,2)
+y2 = np.power(x,3)
+plt.figure(figsize=(12,5)) # Setting the figure size 
+ax = plt.axes()
+ax.set_facecolor("darkgrey") # Setting the background color by using Hex code 
+plt.plot(x,y1,"bo-", x,y2, "ro-")
+plt.xlabel("X - Axis")
+plt.ylabel("Y - Axis")
 plt.show()
