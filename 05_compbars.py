452f0661bd15d75055841c6a31d763c17555a9e7
castobary <46651885+castobary@users.noreply.github.com>
2022-05-10 06:44:09 +0300

    Grouped Bar Chart

--- 05_compbars.py
+++ 05_compbars.py
@@ -45,3 +45,27 @@ plt.ylabel('$Y$')
 plt.title ('$Bar $ $ Chart$')
 plt.legend()
 plt.show()
+
+# Grouped Bar Chart
+plt.figure(figsize=(7,9)) # set width of bar
+barWidth = 0.25
+# set height of bar
+y1= np.array([17,18,29,40])
+y2 =np.array([20,21,22,23])
+y3 =np.array([5,9,11,12])
+# Set position of bar on X axis
+pos1 = np.arange(len(y1))
+pos2 = [x + barWidth for x in pos1]
+pos3 = [x + barWidth for x in pos2]
+# Make the plot
+plt.bar(pos1, y1, color='#FBC02D', width=barWidth, label='Open') 
+plt.bar(pos2, y2, color='#F57F17', width=barWidth, label='Closed') 
+plt.bar(pos3, y3, color='#E65100', width=barWidth, label='Cancelled')
+# Add xticks on the middle of the group bars
+plt.xlabel('Assignee', fontweight='bold')
+plt.ylabel('Number of Tickets', fontweight='bold')
+plt.xticks([i + barWidth for i in range(len(y1))], ['Cassandra','Cherly','Alice','Alex'])
+# Create legend & Show graphic
+plt.legend()
+plt.show()
+np.arange(len(y1))
